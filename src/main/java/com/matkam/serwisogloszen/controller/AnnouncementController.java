ce5e296f72b42f02700cfbde8b8b146f85e8f7e8
Mateusz Bielecki <bielakb98@gmail.com>
2021-05-29 21:58:38 +0200

    abcde

--- src/main/java/com/matkam/serwisogloszen/controller/AnnouncementController.java
+++ src/main/java/com/matkam/serwisogloszen/controller/AnnouncementController.java
@@ -2,17 +2,19 @@ package com.matkam.serwisogloszen.controller;
 
 import com.matkam.serwisogloszen.model.Announcement;
 import com.matkam.serwisogloszen.model.Category;
-import com.matkam.serwisogloszen.model.UserApp;
 import com.matkam.serwisogloszen.service.AnnouncementService;
 import com.matkam.serwisogloszen.service.CategoryService;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
@@ -31,6 +33,7 @@ public class AnnouncementController extends VerticalLayout implements BeforeEnte
     private final AnnouncementService announcementService;
     private final CategoryService categoryService;
 
+
     AnnouncementController(AnnouncementService announcementService, CategoryService categoryService) {
         this.announcementService = announcementService;
         this.categoryService = categoryService;
@@ -42,13 +45,12 @@ public class AnnouncementController extends VerticalLayout implements BeforeEnte
             this.id = Long.parseLong(event.getRouteParameters().get("announcementId").get());
         if (this.id != null) getLayoutAnnouncement();
         else getLayoutAnnouncements();
-
-        UserApp loggedUser = LOGGED_USER;
-        add(new Label(loggedUser.getUsername()));
     }
 
     private void getLayoutAnnouncements() {
         this.removeAll();
+        Label userName = new Label("Witaj "+LOGGED_USER.getUsername());
+        add(userName);
         List<Announcement> announcements = announcementService.findAllAnnouncements();
 
         Grid<Announcement> grid = new Grid<>();
@@ -59,22 +61,30 @@ public class AnnouncementController extends VerticalLayout implements BeforeEnte
         grid.addItemClickListener(event ->
                 UI.getCurrent().getPage().setLocation("http://localhost:8081/announcements/" + event.getItem().getId()));
         add(grid);
-        TextField content = new TextField("Content");
+        TextArea content = new TextArea("Content");
 
         Select<String> select = new Select<>();
         select.setLabel("Category");
         List<Category> categories = categoryService.findAllCategories();
         select.setItems(categories.stream().map(Category::getName));
 
-        add(new VerticalLayout(
+
+        VerticalLayout vl = new VerticalLayout(
                 new H2("Add announcement"),
                 content,
                 select,
                 new Button("Send", event -> addAnnouncement(
                         content.getValue(),
                         select.getValue()
-                )))
-        );
+                )));
+        vl.setWidth(300,Unit.PIXELS);
+        vl.setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        vl.setHorizontalComponentAlignment(Alignment.CENTER);
+        vl.setAlignSelf(Alignment.CENTER);
+        add(vl);
+
+
+
 
     }
 
@@ -90,7 +100,7 @@ public class AnnouncementController extends VerticalLayout implements BeforeEnte
             Notification.show("Enter a category");
         } else {
             Category c = categoryService.findByName(category);
-            announcementService.saveAnnouncement(new Announcement(content, c));
+            announcementService.saveAnnouncement(new Announcement(content, c, LOGGED_USER));
             Notification.show("Added announcement!");
             getLayoutAnnouncements();
 
